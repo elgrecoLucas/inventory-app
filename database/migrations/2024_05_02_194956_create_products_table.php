32f61428fd97ebe28b0c70adbaba79c397773fa6
elgrecoLucas <lucasgustavo8830@gmail.com>
2024-05-02 18:02:19 -0300

    some migrations

---
+++ database/migrations/2024_05_02_194956_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * | is_active lo deshabilito, no puedo cargar más stock para ese producto
+     * | is_featured es para agregarlo a una sección premium/destacarlo
+     * | in_stock SE PUEDE ELIMINAR...NO ESTOY SEGURO SI ES ÚTIL
+     * | on_sale si esta en oferta
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained('tags')->cascadeOnDelete();
+            $table->string('name');
+            $table->json('images')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('model')->nullable();
+            $table->decimal('price');
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('in_stock')->default(true);
+            $table->boolean('on_sale')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
